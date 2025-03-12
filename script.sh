c2cebb02e694d4b1c0a886f10538c28dfd276972
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-03-13 00:36:56 +0330

    variable and symbolic const in bash script

--- script.sh
+++ script.sh
@@ -7,3 +7,8 @@
 # Description: first script
 # Usage: ./scripts (if this program has execute chmod)
 ##################################
+
+name="amir"
+AGE=20 # this type look we must dont change value of this variable (symbolic and means if we change this variable, thats dont give error)
+
+echo "hello , my name is ${name}, Im $(( $age + 2 )) years old"
