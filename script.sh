570ef17c45c5c35ff7c5f85da50b599f69df40e8
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-03-12 23:26:06 +0330

    this commmit show how we can write better bash

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
-echo "hello world"
+#!/bin/bash
+
+##################################
+# AUthor: amirsalar
+# Version: 1.0.0
+# Date: 2025-Mar-12
+# Description: first script
+# Usage: ./scripts (if this program has execute chmod)
+##################################
