e07676b80842c16cb70e598cb6b404603ed46ee7
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-03-14 19:02:48 +0330

    if else elif

--- script.sh
+++ script.sh
@@ -9,6 +9,26 @@
 ##################################
 
 name="amir"
-AGE=20 # this type look we must dont change value of this variable (symbolic and means if we change this variable, thats dont give error)
+age=20 # this type look we must dont change value of this variable (symbolic and means if we change this variable, thats dont give error)
 
 echo "hello , my name is ${name}, Im $(( $age + 2 )) years old"
+
+
+if [ $name == "ali" ]
+then
+	echo " fadat shaaaaam!  XD "
+fi
+
+if [ $age == 21 ]
+then
+	echo " you are not here "
+elif [ $name == "amir" ]
+then
+	echo "mybe you are here so i will go to another test"
+	if [ $age == 20 ]
+	then
+		echo "yeeees!"
+	else
+		echo "nooooooo!"
+	fi
+fi
