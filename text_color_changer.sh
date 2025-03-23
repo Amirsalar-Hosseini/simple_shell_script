c51317cbadfc354d0ff3406e06585fdae3f5ef8d
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-03-23 20:22:13 +0330

    text color changer created

---
+++ text_color_changer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+colorful(){
+	if [[ $# -ne 2 ]]; then
+		echo 'two arguments'
+		exit 1
+	fi
+
+	local string=$1
+	local color=$2
+
+	if [[ $color = 'red' ]]; then
+		local color_code='\e[31m'
+	elif [[ $color = 'blue' ]]; then
+		local color_code='\e[34m'
+	elif [[ $color = 'green' ]]; then
+		local color_code='\e[32m'
+	else
+		local color_code='\e[39m'
+	fi
+
+	echo -e ${color_code}${string}
+}
+
+read -p "whats your string? : " string
+read -p "what color do you want? : " color
+
+colorful $string $color
