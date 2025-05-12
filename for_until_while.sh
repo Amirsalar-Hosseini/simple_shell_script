7ce1f80b278a02511f625024c389589bd2cc2787
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-05-12 14:20:11 +0330

    update for_until_while (input without key sensitive :D)

--- for_until_while.sh
+++ for_until_while.sh
@@ -2,30 +2,30 @@
 
 
 
-# while true; do
-#	read -p "everything ok?" reply
+ while true; do
+	read -p "everything ok?" reply
+
+	if [[ ${reply,,} == "yes" || ${reply,,} == "y" ]]; then
+		echo "pas bro safa kon"
+		exit 0
+	elif [[ ${reply} == "no" || ${reply,,} == "n" ]]; then
+		echo "KILL YOURSELF :D"
+		exit
+	else
+		echo "speak human language...."
+	fi
+ done
+
+
+#counter=$1
 #
-#	if [[ ${reply} == "yes" ]]; then
-#		echo "pas bro safa kon"
-#		exit 0
-#	elif [[ ${reply} == "no" ]]; then
-#		echo "KILL YOURSELF :D"
-#		exit
-#	else
-#		echo "speak human language...."
-#	fi
-# done
-
-
-counter=$1
-
-until [[ ${counter} -gt $2 ]]; do
-	echo "${counter} noch"
-	(( counter++ ))
-done
-
-names=$3
-
-for name in $names; do
-	echo "${name} is cow"
-done
+#until [[ ${counter} -gt $2 ]]; do
+#	echo "${counter} noch"
+#	(( counter++ ))
+#done
+#
+#names=$3
+#
+#for name in $names; do
+#	echo "${name} is cow"
+#done
