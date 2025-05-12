96587de7c8985ec0325c82bc8e43bc7510a63711
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-05-12 11:23:41 +0330

    am_i_allow scripts created

---
+++ am_i_allow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name=$1
+
+case $name in
+  amir)
+    echo "amir, access";;
+  salar)
+    echo "salar, access";;
+  ali)
+    echo "ali, access";;
+  reza)
+    echo "reza, access";;
+  *)
+    echo "denied O_o"
+esac
